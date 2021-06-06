5c10bbf9f6a332e68a59aed763ec568da684b5c5
kennylam91 <phamvanlam1991@gmail.com>
2021-06-06 17:55:41 +0700

    using demo to stop re-rendering child component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Dashboard() {
+  const [textInput, setTextInput] = useState();
   return (
     <div>
       <h2>Dashboard</h2>
+      <h3>Use memo to stop re-render child component</h3>
+      <textarea rows="5" value={textInput} onChange={(event) => setTextInput(event.target.value)} />
+      
     </div>
   )
 }
