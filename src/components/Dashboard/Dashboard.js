738b7777b06b34ef90bdc8cc10bbdad8ff5632e7
kennylam91 <phamvanlam1991@gmail.com>
2021-06-06 18:40:56 +0700

    add a toggle button to show/hide costly component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import CharacterMap from '../CharacterMap/CharacterMap';
 
 export default function Dashboard() {
   const [textInput, setTextInput] = useState('');
+  const [showResult, toggleShowResult] = useReducer(state => !state, false);
   return (
     <div>
       <h2>Dashboard</h2>
       <h3>Use memo to stop re-render child component</h3>
       <textarea rows="5" value={textInput} onChange={(event) => setTextInput(event.target.value)} />
       <br/>
-      <CharacterMap textInput={textInput}/>
+      <button onClick={() => toggleShowResult()} >Show result</button>
+      {showResult &&<CharacterMap textInput={textInput}/> }
     </div>
   )
 }
